fde8e1189bdd9f9e30825dfff27527e70d2571be
Widerone <prins@inbox.ru>
2020-05-01 14:10:47 +0700

    clock

--- js/script.js
+++ js/script.js
@@ -36,4 +36,60 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    //Timer
+    let deadLine = '2020-05-02';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            hours = Math.floor(t / (1000 * 60 * 60)),
+            minutes = Math.floor(((t / 1000) / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        if (Date.parse(deadLine) < Date.parse(new Date())) {
+            minutes = '00';
+            hours = '00';
+            seconds = '00';
+        }
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+    function setClock(id, endtime) {
+
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+        timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = `0${t.hours}`.slice(-2);
+            minutes.textContent = `0${t.minutes}`.slice(-2);
+            seconds.textContent = `0${t.seconds}`.slice(-2);
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+            // TODO время прошло, результат -
+
+        }
+    }
+
+    setClock('timer', deadLine)
+
+    // while (hours != 0) {
+    //     if (seconds > 0)
+    // };
 });
+
+
+/* <div class="timer-numbers" id="timer">
+<span class="hours">18</span>
+<span>:</span>
+<span class="minutes">20</span>
+<span>:</span>
+<span class="seconds">11</span>
+</div> */
